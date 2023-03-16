592c3845625a52510ae8fbe77e2fdf0a6e024ad9
MaryKaheti <kahetikihara@gmail.com>
2023-03-16 12:11:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+function saturdayFun(activity) {
+    if(activity == null){
+        return 'This Saturday, I want to roller-skate!'
+    }else{
+        return 'This Saturday, I want to bathe my dog!'
+    }
+    
+}
+
+function mondayWork(action) {
+    if(action == null){
+        return 'This Monday, I will go to the office.'
+    }else{
+        return 'This Monday, I will work from home.'
+    }
+    
+}
+function wrapAdjective(string) {
+    function inner(special) {
+        if (string === "*") {
+            return `You are *${special}*!`;
+        } else {
+            return `You are ||${special}||!`;
+        }
+    }
+    return inner;
+
+    
+
+
+} 
+
